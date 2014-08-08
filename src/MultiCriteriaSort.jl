c140f5c7229714d355f78fff25d286ee21e49a52
Duane Wilson <duane@analyzere.com>
2014-08-08 12:08:58 -0300

    Correcting formatting

--- src/MultiCriteriaSort.jl
+++ src/MultiCriteriaSort.jl
@@ -1,16 +1,16 @@
 module MultiCriteriaSort
 
-  import Base: sort!, sort, sortperm
-  import Base: lt
+    import Base: sort!, sort, sortperm
+    import Base: lt
 
-  using Base.AbstractVecOrMat
-  using Base.Sort
-  using Base.Sort: InsertionSortAlg, QuickSortAlg, MergeSortAlg
-  using Base.Order
-  using Base.Order.ord
+    using Base.AbstractVecOrMat
+    using Base.Sort
+    using Base.Sort: InsertionSortAlg, QuickSortAlg, MergeSortAlg
+    using Base.Order
+    using Base.Order.ord
 
-  export sort!, sort, sortperm
+    export sort!, sort, sortperm
 
-  include("ordering.jl")
-  include("sort.jl")
+    include("ordering.jl")
+    include("sort.jl")
 end
--- src/sort.jl
+++ src/sort.jl
@@ -1,4 +1,4 @@
-#===============================================================================
+# ==============================================================================
 # Row/Column Swapping Functions
 function swap_slice!(arr::AbstractMatrix, i::Int, j::Int, dim::Int, dim_size::Int)
     if dim == 1
@@ -8,39 +8,39 @@ function swap_slice!(arr::AbstractMatrix, i::Int, j::Int, dim::Int, dim_size::In
     end
 end
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 function swap_row!(arr::AbstractMatrix, i::Int, j::Int, n::Int)
-  for col = 1:n
-    arr[i, col], arr[j, col] = arr[j, col], arr[i, col]
-  end
-end
-
-#-------------------------------------------------------------------------------
-function swap_column!(arr::AbstractMatrix, i::Int, j::Int, m::Int)
-  for row = 1:m
-    arr[row, i], arr[row, j] = arr[row, j], arr[row, i]
-  end
-end
-
-#-------------------------------------------------------------------------------
-function replace_slice!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
-                           i::Int, dim::Int, dim_size::Int)
-  if dim == 1
-    return replace_row!(arr, r, i, dim_size)
-  else
-    return replace_column!(arr, r, i, dim_size)
-  end
-end
-
-#-------------------------------------------------------------------------------
-function replace_row!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
-                         i::Int, n::Int)
     for col = 1:n
-      arr[i, col] = r[col]
+        arr[i, col], arr[j, col] = arr[j, col], arr[i, col]
     end
 end
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+function swap_column!(arr::AbstractMatrix, i::Int, j::Int, m::Int)
+    for row = 1:m
+        arr[row, i], arr[row, j] = arr[row, j], arr[row, i]
+    end
+end
+
+# ------------------------------------------------------------------------------
+function replace_slice!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
+                           i::Int, dim::Int, dim_size::Int)
+    if dim == 1
+        return replace_row!(arr, r, i, dim_size)
+    else
+        return replace_column!(arr, r, i, dim_size)
+    end
+end
+
+# ------------------------------------------------------------------------------
+function replace_row!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
+                         i::Int, n::Int)
+    for col = 1:n
+        arr[i, col] = r[col]
+    end
+end
+
+# ------------------------------------------------------------------------------
 function replace_column!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
                             i::Int, m::Int)
     for row = 1:m
@@ -48,7 +48,7 @@ function replace_column!{T}(arr::AbstractMatrix{T}, r::AbstractMatrix{T},
     end
 end
 
-#===============================================================================
+# ==============================================================================
 # Sorting Algorithms
 function sort!(arr::AbstractMatrix, lo::Int, hi::Int, ::InsertionSortAlg,
                o::Ordering, keys::Vector{Int}, dim::Int)
@@ -73,46 +73,46 @@ function sort!(arr::AbstractMatrix, lo::Int, hi::Int, ::InsertionSortAlg,
     return arr
 end
 
-#--------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 function sort!(arr::AbstractMatrix, lo::Int, hi::Int, a::QuickSortAlg,
                o::Ordering, keys::Vector{Int}, dim::Int)
 
-  if dim > 2 || dim < 1
-    error("Only 2 dimensional arrays are supported")
-  end
+    if dim > 2 || dim < 1
+        error("Only 2 dimensional arrays are supported")
+    end
 
-  dim_size = dim == 1 ? size(arr, 2) : size(arr, 1)
+    dim_size = dim == 1 ? size(arr, 2) : size(arr, 1)
 
-  @inbounds while lo < hi
-    hi-lo <= SMALL_THRESHOLD && return sort!(arr, lo, hi, SMALL_ALGORITHM,
+    @inbounds while lo < hi
+        hi-lo <= SMALL_THRESHOLD && return sort!(arr, lo, hi, SMALL_ALGORITHM,
                                              o, keys, dim)
-    mi = (lo+hi)>>>1
-    if mc_lt(o, arr, mi, lo, keys, dim)
-      swap_slice!(arr, mi, lo, dim, dim_size)
+        mi = (lo+hi)>>>1
+        if mc_lt(o, arr, mi, lo, keys, dim)
+            swap_slice!(arr, mi, lo, dim, dim_size)
+        end
+        if mc_lt(o, arr, hi, mi, keys, dim)
+            swap_slice!(arr, hi, mi, dim, dim_size)
+        end
+        if mc_lt(o, arr, mi, lo, keys, dim)
+            swap_slice!(arr, mi, lo, dim, dim_size)
+        end
+        swap_slice!(arr, mi, lo, dim, dim_size)
+        i, j = lo, hi
+        while true
+            i += 1; j -= 1;
+            while mc_lt(o, arr, i, lo, keys, dim); i += 1; end;
+            while mc_lt(o, arr, lo, j, keys, dim); j -= 1; end;
+            i >= j && break
+            swap_slice!(arr, i, j, dim, dim_size)
+        end
+        swap_slice!(arr, j, lo, dim, dim_size)
+        lo < (j-1) && sort!(arr, lo, j-1, a, o, keys, dim)
+        lo = j+1
     end
-    if mc_lt(o, arr, hi, mi, keys, dim)
-      swap_slice!(arr, hi, mi, dim, dim_size)
-    end
-    if mc_lt(o, arr, mi, lo, keys, dim)
-      swap_slice!(arr, mi, lo, dim, dim_size)
-    end
-    swap_slice!(arr, mi, lo, dim, dim_size)
-    i, j = lo, hi
-    while true
-      i += 1; j -= 1;
-      while mc_lt(o, arr, i, lo, keys, dim); i += 1; end;
-      while mc_lt(o, arr, lo, j, keys, dim); j -= 1; end;
-      i >= j && break
-      swap_slice!(arr, i, j, dim, dim_size)
-    end
-    swap_slice!(arr, j, lo, dim, dim_size)
-    lo < (j-1) && sort!(arr, lo, j-1, a, o, keys, dim)
-    lo = j+1
-  end
-  return arr
+    return arr
 end
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 function sort!(arr::AbstractMatrix, lo::Int, hi::Int, a::MergeSortAlg,
                o::Ordering, keys::Vector{Int}, dim::Int, t=similar(arr))
 
@@ -157,11 +157,11 @@ function sort!(arr::AbstractMatrix, lo::Int, hi::Int, a::MergeSortAlg,
 
     return arr
 end
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 defalg(m::AbstractMatrix) = DEFAULT_STABLE
 defalg{T<:Number}(m::AbstractMatrix{T}) = DEFAULT_UNSTABLE
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 sort!(m::AbstractMatrix, alg::Algorithm, order::Ordering, keys::Vector{Int},
       dim::Int = 1) =  sort!(m, 1, size(m, dim), alg,
                           order, keys, dim)
@@ -172,7 +172,7 @@ sort!(m::AbstractMatrix, keys::Vector{Int}; alg::Algorithm=defalg(m),
 
 sort(m::AbstractMatrix, keys::Vector{Int}; kws...) = sort!(copy(m), keys; kws...)
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 sortperm(m::AbstractMatrix, keys::Vector{Int}; alg::Algorithm=defalg(m),
          lt::Function=isless, by::Function=identity, rev::Bool=false,
          order::Ordering=Forward, dim::Int = 1) =
